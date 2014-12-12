fd13051789b21deb9585d3a3f622484593bbf3a3
Angel Gutierrez Monola <gutierrez.monola@gmail.com>
2014-12-12 12:12:30 -0600

    Acelerometro test

--- themes/js/testing.js
+++ themes/js/testing.js
@@ -4,4 +4,17 @@ function onBodyLoad(){
 
 function onDeviceReady(){
 	$("#resultado").html("PhoneGap esta en marcha");
+	
+	navigator.accelerometer.getCurrentAcceleration(onInfo, onError);
 }
+
+function onInfo(acceleration) {
+    $("#resultado").append('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+};
+
+function onError() {
+    alert('onError!');
+};
