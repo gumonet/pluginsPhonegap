01fc1b0851816b0ebd79c969fa3144ce9e16e9d8
Angel Gutierrez Monola <gutierrez.monola@gmail.com>
2014-12-12 12:59:20 -0600

    xx

--- themes/js/testing.js
+++ themes/js/testing.js
@@ -6,16 +6,20 @@ function onDeviceReady(){
 	$("#resultado").html("PhoneGap esta en marcha");
 	
 	//navigator.accelerometer.getCurrentAcceleration(onInfo, onError);
-	var watch = navigator.accelerometer.watchAcceleration(onInfo,OnError,{frequency:2000});
+	//var watch = navigator.accelerometer.watchAcceleration(onInfo,OnError,{frequency: 3000 });
+	var watchID= navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 3000 });
 }
 
-function onInfo(acceleration) {
-    $("#resultado").html('Acceleration X: ' + acceleration.x + '<br>' +
-          'Acceleration Y: ' + acceleration.y + '<br>' +
-          'Acceleration Z: ' + acceleration.z + '<br>' +
-          'Timestamp: '      + acceleration.timestamp + '<br>');
+function onSuccess(acceleration) {
+    $("#resultado").html('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
 };
 
 function onError() {
     alert('onError!');
 };
+
+
+
